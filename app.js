a10c84e454bece17f0bbc744bea4bbac27f416f3
Yeffry-Fermin <anthonybatista291@gmail.com>
2026-06-24 14:45:58 -0400

    finish part 7

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-function middlewareLogger(req, res, next) {
-  console.log(req.method);
-  console.log(req.originalUrl);
-  next();
-}
+// function middlewareLogger(req, res, next) {
+//   console.log(req.method);
+//   console.log(req.originalUrl);
+//   next();
+// }
 
 const express = require("express");
 const apiRouter = require("./api/index");
+const morgan = require('morgan')
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(middlewareLogger);
+app.use(morgan("dev"))
+// app.use(middlewareLogger);
 app.use("/api", apiRouter);
 
 function errorHandler(err, req, res, next) {
