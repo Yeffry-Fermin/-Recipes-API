f1343198619910c465d221d160960103d26b6422
Yeffry-Fermin <anthonybatista291@gmail.com>
2026-06-24 14:49:41 -0400

    finished part 8, complete

--- app.js
+++ app.js
@@ -7,11 +7,17 @@
 const express = require("express");
 const apiRouter = require("./api/index");
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(cors())
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// })
 // app.use(middlewareLogger);
 app.use("/api", apiRouter);
 
